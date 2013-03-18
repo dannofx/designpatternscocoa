d2d4ca8a564b718e00c7b2a1ff3bcfbabe8568d7
dannofx <danno.fx@gmail.com>
2013-03-18 01:44:40 -0600

    Inicio de ejercicio Factory pattern

--- FactoryPatterniOS/FactoryPatterniOS/ViewController.m
+++ FactoryPatterniOS/FactoryPatterniOS/ViewController.m
@@ -18,13 +18,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 #pragma mark - User actions
