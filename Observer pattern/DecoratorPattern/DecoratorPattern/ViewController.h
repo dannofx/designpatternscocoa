44420b86ad6017e685e65d20581384e2740f6253
dannofx <danno.fx@gmail.com>
2013-04-08 02:33:20 -0500

    Ejercicio decorator comenzado

---
+++ Observer pattern/DecoratorPattern/DecoratorPattern/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DecoratorPattern
+//
+//  Created by Danno on 08/04/13.
+//  Copyright (c) 2013 Danno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
