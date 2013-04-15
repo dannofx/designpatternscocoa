e3987e8fc5fd3d9bedcbe0e3b7c919375fd361ef
dannofx <danno.fx@gmail.com>
2013-04-15 02:58:10 -0500

    Ejercicio proxy completo

---
+++ ProxyPattern/ProxyPattern/CatAsyncView.h
@@ -0,0 +1,13 @@
+//
+//  CatAsyncView.h
+//  ProxyPattern
+//
+//  Created by Danno on 15/04/13.
+//  Copyright (c) 2013 Danno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CatAsyncView : UIImageView
+
+@end
---
+++ ProxyPattern/ProxyPattern/CatAsyncView.m
@@ -0,0 +1,31 @@
+//
+//  CatAsyncView.m
+//  ProxyPattern
+//
+//  Created by Danno on 15/04/13.
+//  Copyright (c) 2013 Danno. All rights reserved.
+//
+
+#import "CatAsyncView.h"
+
+@implementation CatAsyncView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
